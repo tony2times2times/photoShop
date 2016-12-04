6d80086ad88e95e167b6d87e4a3af0b6f1d00bde
tony2times2times <tony.peraza86@gmail.com>
2016-12-04 17:48:23 -0600

    Added total student count and improved display

---
+++ README.md
@@ -0,0 +1,30 @@
+Weekend Challenge 02
+====================
+
+import JSON data file
+http://devjana.net/support/tau_students.json
+
+[x] make an AJAX call from the client side app.js, using an .
+[x] one person at a time represented - the info for the first person in the json data.
+[x] One the screen should also be Prev and Next buttons, that when pressed, show the information for the appropriate person.
+[x] These should wrap - prev when on the first person should wrap around to show the last person and vice versa.
+
+[x] Also on the DOM should be a display showing the number of people and which is being currently viewed (eg. 2/20)
+
+[x] When a person is displayed, show their name (first & last) and their info. Only one person should be shown at any given time.
+
+KINDA HARD MODE
+---------------
+
+[] Add a button for each person - appended to DOM when the json is read in. Clicking any button will display that person. Place these between the prev and next buttons.
+
+
+HARD MODE
+---------
+
+[] Include a fade out and fade in animation in-between transitioning people.
+
+PRO MODE
+--------
+
+[] Include a timer that moves to the next person if the user is not clicking on next or prev. If the user clicks on next or prev, the timer should be reset. The timer should transition between people every 10 seconds.
--- scripts/clientScripts.js
+++ scripts/clientScripts.js
@@ -4,51 +4,73 @@ var roster = [];
 var count = 0;
 
 $(document).ready(function functionName() {
-    if (debug){console.log('Document standing by!');}
+    if (debug) {
+        console.log('Document standing by!');
+    }
 
     function getRoster() {
-      $('#back').click(function(){
-        if (debug){console.log('current student number is : ' + count + ' subtracting 1');}
-        if (count>0) {
-          count--;
-        }
-        else{
-          count = roster.length-1;
-        }
-        if (debug){console.log('Student number is now : ' + count);}
-        displayStudent();
-      });
-
-      $('#forward').click(function(){
-        if (debug){console.log('current student number is : ' + count + ' adding 1');}
-        if (count<(roster.length - 1)) {
-          count++;
-        }
-        else{
-          count = 0;
-        }
-        if (debug){console.log('Student number is now : ' + count);}
-        displayStudent();
-      });
-
         $.ajax({
             url: 'http://devjana.net/support/tau_students.json',
             dataType: 'JSON',
             success: function(data) {
-              if (debug){console.log('Data standing by!');}
+                if (debug) {
+                    console.log('Data standing by!');
+                }
                 roster = data.tau;
                 console.log(roster);
                 displayStudent();
+                displayNav();
             }
         });
     }
 
     function displayStudent() {
-      if (debug){console.log('displayStudent standing by!');}
-        $('#tau').html(
-        '<p class="name">' +roster[count].first_name +'!'+ roster[count].last_name  +'</p>'+'<br>'+
-        '<img src="'+roster[count].picUrl+'"</img>' +'<br>'+
-        'About me: ' + roster[count].info );
+        if (debug) {
+            console.log('displayStudent standing by!');
+        }
+        var nameDisplay = '<p class="name">' + roster[count].first_name + '!' + roster[count].last_name + '</p>' + '<br>';
+        var picDisplay = '<img src="' + roster[count].picUrl + '"</img>' + '<br>';
+        var infoDisplay = '<p class="info">About me: ' + roster[count].info + '</p>';
+        var countDisplay = '<p id="count">' + (count + 1) + ' / ' + roster.length + '</p>' + '<br>';
+        $('#tau').html(nameDisplay + picDisplay + infoDisplay + countDisplay);
+        }
+
+    function displayNav() {
+        var studentNames = [];
+        for (var i = 0; i < roster.length; i++) {
+           studentNames[i] = '<button class=student_buttons>'+
+           roster[i].first_name +' '+ roster[i].last_name + '</button>';
+        }
+        if (debug) {console.log('Student names: ' + studentNames);}
+        $('#studentButtons').html(studentNames);
+        $('#back').click(function() {
+            if (debug) {
+                console.log('current student number is : ' + count + ' subtracting 1');
+            }
+            if (count > 0) {
+                count--;
+            } else {
+                count = roster.length - 1;
+            }
+            if (debug) {
+                console.log('Student number is now : ' + count);
+            }
+            displayStudent();
+        });
+        $('#forward').click(function() {
+            if (debug) {
+                console.log('current student number is : ' + count + ' adding 1');
+            }
+            if (count < (roster.length - 1)) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (debug) {
+                console.log('Student number is now : ' + count);
+            }
+            displayStudent();
+        });
     }
-        getRoster();
+    getRoster();
 });
