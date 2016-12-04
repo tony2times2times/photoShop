115f973632b9b2d2cda35e8659e3e708585a73d5
Tony Peraza <tony.peraza86@gmail.com>
2016-12-03 18:54:13 -0600

    display students tothe DOM

--- scripts/clientScripts.js
+++ scripts/clientScripts.js
@@ -1,30 +1,54 @@
 console.log('JS standing by!');
-
+var debug = true;
 var roster = [];
-var count = 2;
+var count = 0;
 
 $(document).ready(function functionName() {
-    console.log('Document standing by!');
+    if (debug){console.log('Document standing by!');}
+
     function getRoster() {
+      $('#back').click(function(){
+        if (debug){console.log('current student number is : ' + count + ' subtracting 1');}
+        if (count>0) {
+          count--;
+        }
+        else{
+          count = roster.length-1;
+        }
+        if (debug){console.log('Student number is now : ' + count);}
+        displayStudent();
+      });
+
+      $('#forward').click(function(){
+        if (debug){console.log('current student number is : ' + count + ' adding 1');}
+        if (count<(roster.length - 1)) {
+          count++;
+        }
+        else{
+          count = 0;
+        }
+        if (debug){console.log('Student number is now : ' + count);}
+        displayStudent();
+      });
+
         $.ajax({
             url: 'http://devjana.net/support/tau_students.json',
             dataType: 'JSON',
             success: function(data) {
-                console.log('Data standing by!');
+              if (debug){console.log('Data standing by!');}
                 roster = data.tau;
                 console.log(roster);
-                displayRoster();
+                displayStudent();
             }
         });
     }
-    function displayRoster() {
-        console.log('displayRoster standing by!');
-        $('#tau').html(roster[count].first_name +' '+ roster[count].last_name  +
-        '<img src="'+roster[count].picUrl+'"</img>' +
+
+    function displayStudent() {
+      if (debug){console.log('displayStudent standing by!');}
+        $('#tau').html(
+        '<p class="name">' +roster[count].first_name +'!'+ roster[count].last_name  +'</p>'+'<br>'+
+        '<img src="'+roster[count].picUrl+'"</img>' +'<br>'+
         'About me: ' + roster[count].info );
-    }
-    function buttonControls(){
-      $(#back).click(function{count--});
     }
         getRoster();
 });
